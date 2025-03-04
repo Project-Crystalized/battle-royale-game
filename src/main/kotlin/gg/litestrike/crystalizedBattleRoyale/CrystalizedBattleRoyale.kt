6b23dd315a925cd59bc9c56f06499ac313476a71
unknown <lucashenriquedg@hotmail.com>
2025-03-04 12:02:26 -0300

    first commit

---
+++ src/main/kotlin/gg/litestrike/crystalizedBattleRoyale/CrystalizedBattleRoyale.kt
@@ -0,0 +1,14 @@
+package gg.litestrike.crystalizedBattleRoyale
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CrystalizedBattleRoyale : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
